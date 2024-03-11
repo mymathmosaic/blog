1f0da1f80bf032288515cf0fe4c4489e5fb18ad4
Tanja <my.math.mosaic@gmail.com>
2024-03-11 14:52:02 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const siteConfig = require('./config.js');
 const postCssPlugins = require('./postcss-config.js');
 
 module.exports = {
-  pathPrefix: siteConfig.pathPrefix,
+  pathPrefix: '/mymathmosaic.github.io', //siteConfig.pathPrefix,
   siteMetadata: {
     url: siteConfig.url,
     title: siteConfig.title,
